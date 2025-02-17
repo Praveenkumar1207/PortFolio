96ded370e47b7366b0f39d635394577958e64d94
Praveenkumar1207 <praveenkumarv1207@outlook.com>
2025-02-17 22:03:58 +0530

    Commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -33,7 +33,7 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 375px)");
+    const mediaQuery = window.matchMedia("(max-width: 7680px)");
     setIsMobile(mediaQuery.matches);
 
     const handleMediaQueryChange = (event) => {
